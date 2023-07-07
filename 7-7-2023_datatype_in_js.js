f67f74426e12715f43393b7b4dcda3746a28f8c5
sricharanputtoju <puttojusricharan@gmail.com>
2023-07-07 22:10:39 +0530

    datatypes

---
+++ 7-7-2023_datatype_in_js.js
@@ -0,0 +1,102 @@
+//numbers
+var a =10;
+console.log(a);
+
+console.log(typeof a);
+
+//strings
+
+var b ="10000";
+
+console.log(b);
+
+console.log(typeof b);
+
+//boolean true or false
+
+var c =true;
+
+console.log(c);
+
+console.log(typeof c);
+
+//undefined
+
+var d;
+
+console.log(d);
+
+console.log(typeof d);
+
+//null
+
+var e =null
+
+console.log(e);
+
+console.log(typeof e);
+
+//Enter javascript is nothing but an objects
+
+var student =[{
+    name:"sricharan",
+    age:23,
+    section:1
+},
+{
+    name:"bharath",
+    age:25,
+    section:2
+},
+{
+    name:"praveen",
+    age:27,
+    section:3
+},
+{
+    name:"srinu",
+    age:22,
+    section:1
+},
+{
+    name:"vamshi",
+    age:24,
+    section:2
+}];
+
+console.log(student);
+
+var studentsricharan ={
+    name:"sricharan",
+    age:23,
+    section:1
+};
+console.log(studentsricharan);
+
+var studentbharath ={
+    name:"bharath",
+    age:25,
+    section:2
+};
+console.log(studentbharath);
+
+var studentpraveen ={
+    name:"praveen",
+    age:27,
+    section:3
+};
+console.log(studentpraveen);
+
+var studentsrinu ={
+    name:"srinu",
+    age:22,
+    section:1
+};
+console.log(studentsrinu);
+
+var studentvamshi ={
+    name:"vamshi",
+    age:24,
+    section:2
+};
+console.log(studentvamshi);
