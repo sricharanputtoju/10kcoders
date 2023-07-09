ae2727407af929fbe28cde44cfa7584cb821cfa3
sricharanputtoju <puttojusricharan@gmail.com>
2023-07-09 22:50:36 +0530

    operators

---
+++ 8-7-2023_operators_in_js.js
@@ -0,0 +1,119 @@
+//Addition
+
+const add = 10 + 20;
+
+console.log(add);
+
+//subtraction
+
+const sub = 40 - 10;
+
+console.log(sub);
+
+//multiplication
+
+const mul = 2 * 34;
+
+console.log(mul);
+
+//divition
+
+const div = 5 / 2;
+
+console.log(div);
+
+//modulus
+
+const mod = 5 % 2;
+
+console.log(mod);
+
+//exponentiation
+
+const expo = 15 ** 2;
+
+console.log(expo);
+
+//=
+
+let a = 10;
+
+//+=
+
+a += 1;
+a += 10;
+a += 11;
+a += 5;
+a += 9;
+
+console.log(a);
+
+//-=
+
+a -= 2;
+a -= 20;
+a -= 8;
+a -= 3;
+a -= 7;
+
+console.log(a);
+
+//*=
+
+a *= 2;
+a *= 3;
+a *= 4;
+a *= 5;
+a *= 6;
+
+console.log(a);
+
+//conditional operators;
+//It will return a boolean values
+
+//>
+console.log(2 > 5);
+console.log(5 > 2);
+console.log(3 > 3);
+console.log(1 > 3);
+console.log(10 > 9);
+
+//<
+console.log(2 < 5);
+console.log(5 < 2);
+console.log(6 < 6);
+console.log(10 < 12);
+console.log(20 < 19);
+
+//>=
+
+console.log(2 >= 2);
+console.log(5 >= 2);
+console.log(3 >= 3);
+console.log(1 >= 3);
+console.log(10 >= 10);
+
+//<=
+console.log(2 <= 5);
+console.log(5 <= 2);
+console.log(6 <= 6);
+console.log(10 <= 12);
+console.log(20 <= 20);
+
+//==
+//checks the value and ingnore datatype
+
+console.log(2 == "2");
+console.log(true == "true");
+console.log(true == "1");
+console.log(false == "0");
+console.log(false == "false");
+
+//===
+//checks the value and datatype
+
+console.log(2 === 2);
+console.log(true === true);
+console.log(true === 1);
+console.log(false === 0);
+console.log(false === false);
